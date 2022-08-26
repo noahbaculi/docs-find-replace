14070ab475bf467f331cc6c65ea3d7f21508bb5c
Noah <nbaculi@hotmail.com>
2022-08-25 19:43:52 -0700

    change to doc_find_replace.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: gunicorn doc_find_replace:app
--- doc_find_replace.py
+++ doc_find_replace.py
@@ -36,35 +36,31 @@ def allowed_file(filename):
 
 
 @app.route("/", methods=["GET", "POST"])
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-# def upload_file():
-#     if request.method == "POST":
-#         # check if the post request has the file part
-#         if "file" not in request.files:
-#             flash("No file part")
-#             return redirect(request.url)
-#         file = request.files["file"]
-#         # if user does not select file, browser also
-#         # submit an empty part without filename
-#         if file.filename == "":
-#             flash("No selected file")
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-#             return redirect(url_for("uploaded_file", filename=filename))
-#     return """
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form method=post enctype=multipart/form-data>
-#       <input type=file name=file>
-#       <input type=submit value=Upload>
-#     </form>
-#     """
+def upload_file():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return redirect(url_for("uploaded_file", filename=filename))
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    """
 
 
 if __name__ == "__main__":
